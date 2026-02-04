c6a6f8353262de8d10ee855c5e31ed45e46e1a98
Anuj Kumar <anujrajput14062001@gmail.com>
2026-02-04 15:05:07 +0530

    Add files via upload

---
+++ churn_model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv("churn.csv")
+
+data['Churn'] = data['Churn'].map({'Yes':1,'No':0})
+
+X = data.drop('Churn', axis=1)
+y = data['Churn']
+
+X = pd.get_dummies(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+print("Accuracy:", model.score(X_test, y_test))
